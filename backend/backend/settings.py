8440b15b564e05f20a1c5c51fa08bf54467bd409
Clement xen <140471915+ClementMabila@users.noreply.github.com>
2025-06-06 22:40:21 +0200

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -140,10 +140,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = os.environ.get('CORS_ALLOW_ALL_ORIGINS', 'False').lower() == 'true'
 CORS_ALLOWED_ORIGINS = os.environ.get(
     'CORS_ALLOWED_ORIGINS',
-    'https://refine-86km4zg4l-clementmabilas-projects.vercel.app',
-    'https://refine-go.vercel.app/',
-    'https://refine-86km4zg4l-clementmabilas-projects.vercel.app/',
-    'http://localhost:3000,http://127.0.0.1:3000'
+    'https://refine-go.vercel.app',
+    'https://refine-go.vercel.app/',,
 ).split(',')
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = [
