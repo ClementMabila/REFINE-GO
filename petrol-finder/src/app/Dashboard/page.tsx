6a67b355553017a87b3754ded082f91f78ea29ef
Clement xen <140471915+ClementMabila@users.noreply.github.com>
2025-06-06 23:27:10 +0200

    Update page.tsx

--- petrol-finder/src/app/Dashboard/page.tsx
+++ petrol-finder/src/app/Dashboard/page.tsx
@@ -311,7 +311,7 @@ const PetrolFinderPage = () => {
   const [user, setUser] = useState<User | null>(null);
   const [SideUser, setSideUser] = useState<SideUser | null>(null);
   const [csrfToken, setCsrfToken] = useState("");
-  const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://refine-go.onrender.com/api';
+  const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://refine-go.onrender.com';
   //User fetch
 
   const filteredStations = useMemo(() => {
