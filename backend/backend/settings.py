c537a8a1721b927da4b726efbaab93259a12a358
Clement xen <140471915+ClementMabila@users.noreply.github.com>
2025-06-06 23:18:14 +0200

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -163,7 +163,7 @@ CSRF_COOKIE_SAMESITE = 'Lax'
 CSRF_USE_SESSIONS = False
 CSRF_TRUSTED_ORIGINS = os.environ.get(
     'CSRF_TRUSTED_ORIGINS',
-    'http://localhost:3000,http://127.0.0.1:3000'
+    'https://refine-go.vercel.app'
 ).split(',')
 
 # Session Configuration
