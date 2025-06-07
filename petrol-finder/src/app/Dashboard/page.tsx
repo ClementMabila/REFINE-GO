4d6fae49d8c7a3feec6f72bb5cc69f5c2e86a3ea
Clement xen <140471915+ClementMabila@users.noreply.github.com>
2025-06-07 18:53:42 +0200

    Update page.tsx

--- petrol-finder/src/app/Dashboard/page.tsx
+++ petrol-finder/src/app/Dashboard/page.tsx
@@ -2673,7 +2673,6 @@ const NavigationButton = () => (
                 onChange={(e) => setSearchQuery(e.target.value)}
                 className={`w-50 pl-10 md:hidden pr-4 py-2 rounded-full border ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-300'} focus:outline-none focus:ring-2 focus:ring-[#2edda2] `}
               />
-              <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
             </div>
             
             {/* Fuel type selection */}
